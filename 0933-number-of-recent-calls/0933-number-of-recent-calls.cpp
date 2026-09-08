0c4b58dad5720236c2bb042531b3ecc7b12dea4f
vtu28343-Ajay <vtu28343@veltech.edu.in>
2026-09-08 15:24:05 +0530

    Time: 8 ms (93.55%), Space: 64.1 MB (69.01%) - LeetHub

---
+++ 0933-number-of-recent-calls/0933-number-of-recent-calls.cpp
@@ -0,0 +1,26 @@
+class RecentCounter {
+private:
+    queue<int> requests;
+
+public:
+    RecentCounter() {
+        
+    }
+    
+    int ping(int t) {
+        requests.push(t);
+        
+        // Remove requests older than t - 3000
+        while (!requests.empty() && requests.front() < t - 3000) {
+            requests.pop();
+        }
+        
+        return requests.size();
+    }
+};
+
+/**
+ * Your RecentCounter object will be instantiated and called as such:
+ * RecentCounter* obj = new RecentCounter();
+ * int param_1 = obj->ping(t);
+ */
