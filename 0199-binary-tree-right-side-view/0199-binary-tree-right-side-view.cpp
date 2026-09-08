a984565900e310d05c686d5a61ee243122b8e5ed
vtu28343-Ajay <vtu28343@veltech.edu.in>
2026-09-08 15:18:55 +0530

    Time: 0 ms (100%), Space: 14.9 MB (85.67%) - LeetHub

---
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<int> rightSideView(TreeNode* root) {
+        vector<int> result;
+        if (!root) return result;
+        
+        queue<TreeNode*> q;
+        q.push(root);
+        
+        while (!q.empty()) {
+            int levelSize = q.size();
+            
+            for (int i = 0; i < levelSize; ++i) {
+                TreeNode* node = q.front();
+                q.pop();
+                
+                // The last element of the current level is visible from the right side
+                if (i == levelSize - 1) {
+                    result.push_back(node->val);
+                }
+                
+                if (node->left) q.push(node->left);
+                if (node->right) q.push(node->right);
+            }
+        }
+        
+        return result;
+    }
+};
